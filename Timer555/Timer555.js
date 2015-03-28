c0d5a98472fc4efb58602ec9560df3f4e7dd827c
MadeInTheUSB <MadeInTheUSB@gmail.com>
2015-03-28 16:28:14 -0400

    Timer 555 update

--- Timer555/Timer555.js
+++ Timer555/Timer555.js
@@ -1,7 +1,41 @@
 /*
+Timer 555, Monostable Mode, one time timer
+========================================================
+http://www.instructables.com/id/555-Timer/step2/555-Timer-Monostable-Mode/
 
-LED Blinker:	
-timer 
+                                        -t / (R*C)
+(Voltage across Capacitor) = Vcc * (1- e          )
+t = 1.1 * R * C
+
+var C = 470uF
+var Ra = 47k => ~10seconds
+
+1. Connect power and ground to pins 8 and 1 of the 555 timer (red and black wires).  
+2. Connect a 0.01uF capacitor between pins 5 and 1.  
+3. Connect a 440uF capacitor between +pin to pin 6 and capacitor -pin to ground, 
+    make sure that the negative lead of the capacitor is connected to pin 1.  
+4. Connect pins 6 and 7 with a jumper wire (green).  
+5. Connect a 47k resistor between pins 7 and 8.  
+6. Connect pins 4 and to Vcc
+7. Connect LED+ to pin 3 and LED- to ground
+8. >>> Connect an LED and current limiting resistor in series from the output of the 555 to ground.  
+9. Connect Wire the momentary push button switch in series with a 10K resistor between Vcc and ground.  
+   Connect a wire (yellow) from the junction between the switch and resistor to the trigger pin 2
+
+*/
+
+function Get10P100(v) {
+	return v*100/1000;
+}
+var capacitor = 47/1000/100  // 470 uF
+var Ra = (10+Get10P100(10)) * 1000 // kOmh
+var oneTimeDuration = 1.1 * Ra * capacitor
+print("oneTimeDuration:"+oneTimeDuration);
+
+/*
+
+
+
 
 Timer 555, 1 LEDs Blinking Schematic:
 =====================================
@@ -20,9 +54,10 @@ Timer 555, 1 LEDs Blinking Schematic:
 Timer 555, 2 LEDs Blinking Schematic:
 =====================================
 
+1. Pin1 to ground
 2. Pin2(trigger) to pin6(Threshold)
 3. Pin8(Vcc) to pin4(Reset)
-4. Pin7(Discharge) to pin8(Vcc) using 22k Ohm resistor
+4. Pin7(Discharge) to pin8(Vcc) using 4.7k Ohm resistor
 5. Connect 47uF Capacitor negative pin to ground
 6. Connect 47uF Capacitor positive pin to pin6 (threshold)
 7. Connect 47uF Capacitor positive pin to pin7(Discharge) using 10k Ohm resistor
@@ -32,7 +67,9 @@ Timer 555, 2 LEDs Blinking Schematic:
 9. Connect Pin8(Vcc) to Power
 
 http://en.wikipedia.org/wiki/555_timer_IC
-Goog explainaintion: http://www.instructables.com/id/555-Timer/step5/555-Timer-Astable-Mode/
+
+555 Timer: Astable Mode
+Good explainaintion: http://www.instructables.com/id/555-Timer/step5/555-Timer-Astable-Mode/
 http://www.instructables.com/id/Know-Your-IC-555-Timers/step3/Project-555-timer-Light-Blinker/
 http://www.instructables.com/id/ThrobbingFading-LED-w555ith-555-Timer/?utm_source=pm&utm_medium=related-instructables&utm_campaign=related_test
 
@@ -79,6 +116,8 @@ function print(s) {
 	console.log(s)
 }
 
+//555 timer oscillator frequency calculator
+
 // Calculator
 // http://houseofjeff.com/555-timer-oscillator-frequency-calculator/#comment-8887
 
@@ -113,3 +152,8 @@ print("dutyCycleSimplified:"+dutyCycleSimplified)
 
 //FrequencyOfOutput = 1 /( 0.7*( 100000 + 2 * 10000) * 47/1000/1000/1000) == 253.2928 ==  25Hz
 //FrequencyOfOutput = 1 /( 0.7*( ra:100,000 + 2 * Rb:10,000) * 0.00000047)
+
+//V = 3.77
+//R=V/I 
+//0.2 mA == With 10kOmh  	=> 3.13v ===> 10 000 = 3.77/0.0002
+//0.03 A => 30mA 5v R=0
