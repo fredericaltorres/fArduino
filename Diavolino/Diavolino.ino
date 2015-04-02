2d4b47375157ea8e289fa37ff8dfa0053b494a04
MadeInTheUSB <MadeInTheUSB@gmail.com>
2015-04-02 00:15:06 -0400

    Diavolino

---
+++ Diavolino/Diavolino.ino
@@ -0,0 +1,48 @@
+/**************************************************************************
+
+http://shop.evilmadscientist.com/productsmenu/tinykitlist/180-diavolino
+ATmega328P
+duemilanove
+
+*************************************************************************/
+
+#include "fArduino.h"
+
+#define ON_BOARD_LED 13
+Led _onBoardLed(ON_BOARD_LED);
+
+#define EX_LED1 2
+Led _exLed1(EX_LED1);
+
+#define EX_LED2 3
+Led _exLed2(EX_LED2);
+
+
+int PHOTO_CELL_PIN = 5;
+
+void setup() {
+
+    Board.Delay(1500);
+    Board.InitializeComputerCommunication(9600, "Initializing...");
+    Board.TraceHeader("Diavolino = Duemilanove(ATmega328P)");
+    
+    Board.SetPinMode(ON_BOARD_LED, OUTPUT);
+    _onBoardLed.SetBlinkMode(2000);
+
+    Board.SetPinMode(EX_LED1, OUTPUT);
+    _exLed1.SetBlinkMode(500);
+
+    Board.SetPinMode(EX_LED2, OUTPUT);
+    _exLed2.SetBlinkMode(500);
+}
+
+void loop() {
+
+    _onBoardLed.Blink();
+    _exLed1.Blink();
+    _exLed2.Blink();
+    
+    Board.Trace(StringFormat.Format("photocellPin(%d):%d", PHOTO_CELL_PIN, analogRead(PHOTO_CELL_PIN)));
+ 
+    Board.Delay(400);
+}
